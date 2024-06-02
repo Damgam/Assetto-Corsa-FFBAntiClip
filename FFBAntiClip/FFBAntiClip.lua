d63e37f8e9efab8c7648c6399c39378a27a1062f
Damgam <c_2_damiano@op.pl>
2024-06-02 06:17:47 +0200

    Update

--- FFBAntiClip/FFBAntiClip.lua
+++ FFBAntiClip/FFBAntiClip.lua
@@ -110,7 +110,7 @@ function script.update(dt)
 
     avgFPS = ((avgFPS*599)+Sim.fps)/600
 
-    if ConfigFFBAntiClipEnabled and (not Sim.isReplayActive) and Car.speedKmh > 50 and (not Sim.isPaused) and (not Car.isAIControlled) then -- FFB Anti-Clip
+    if ConfigFFBAntiClipEnabled and (not Sim.isReplayActive) and Car.speedKmh > 50 and (not Sim.isPaused) and (not Car.isAIControlled) and ac.getSim().inputMode == 0 then -- FFB Anti-Clip
         FFBAntiClipFunction()
     end
 
